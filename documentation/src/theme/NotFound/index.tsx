795aac63b03333d34a17fab0c60a2f31e03d63b2
Faridekhalili <khalili.f@northeastern.edu>
2024-10-13 14:45:05 -0400

    fix reset leftover.

--- documentation/src/theme/NotFound/index.tsx
+++ documentation/src/theme/NotFound/index.tsx
@@ -1,25 +1,27 @@
 import { memo } from "react";
-import React from 'react';
-import { translate } from '@docusaurus/Translate';
-import { PageMetadata } from '@docusaurus/theme-common';
-import Layout from '@theme/Layout';
-import NotFoundContent from '@theme/NotFound/Content';
+import React from "react";
+import { translate } from "@docusaurus/Translate";
+import { PageMetadata } from "@docusaurus/theme-common";
+import Layout from "@theme/Layout";
+import NotFoundContent from "@theme/NotFound/Content";
 export default memo(function Index() {
   console.log(window.globalCount++);
   const title = translate({
-    id: 'theme.NotFound.title',
-    message: 'Page Not Found'
+    id: "theme.NotFound.title",
+    message: "Page Not Found",
   });
-  return <>
+  return (
+    <>
       <PageMetadata title={title} />
       <Layout>
         <NotFoundContent />
       </Layout>
-    </>;
+    </>
+  );
 });
 declare global {
   interface Window {
     globalCount: number;
   }
 }
-window.globalCount = 0;
+window.globalCount = window.globalCount || 0;
